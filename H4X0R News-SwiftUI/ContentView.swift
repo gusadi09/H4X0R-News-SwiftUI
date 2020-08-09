15dd605d7454c5663c5a86cb6ccc6eff08f13700
gusadi09 <gusadi872@gmail.com>
2020-08-09 23:06:32 +0800

    Initial Commit

---
+++ H4X0R News-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4X0R News-SwiftUI
+//
+//  Created by Gus Adi on 09/08/20.
+//  Copyright © 2020 Gusadi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
